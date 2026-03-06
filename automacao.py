344467344c5ca63a8c28b9ee1b68605016eb4fde
anaviboahoras <anavboahorasantos@gmail.com>
2026-03-06 15:02:03 -0300

    pandas

--- automacao.py
+++ automacao.py
@@ -1,8 +1,9 @@
 # Automacao de Atendimento ao Cliente
 #Este script automatiza o registro de produtos
 
-import pyautogui
 import time
+import pyautogui
+import pandas
 #pyautogui.click-> Clica em um ponto específico da tela
 #pyautogui.write-> Escreve um texto
 #pyautogui.press -> Pressiona uma tecla
@@ -19,13 +20,18 @@ pyautogui.press("enter") # Pressiona "Enter" para acessar o site
 time.sleep(3) # Aguarda 3 segundos para o site carregar
 
 #Passo 2: Fazer login
-time.sleep(1) # Aguarda 1 segundo
+time.sleep(2) # Aguarda 2 segundos para garantir que o campo de email esteja visível
 pyautogui.click(x=515, y=403) # Clica no campo de email
 pyautogui.write("seuemail@gmail.com") # Escreve o email
 pyautogui.press("tab") # Pressiona "Tab" para ir para o campo de senha
 pyautogui.write("45667865424bl") # Escreve a senha
+pyautogui.press("tab") # Pressiona "Tab" para ir para o botão de login
+pyautogui.press("enter") # Pressiona "Enter" para fazer login
 
-#Passo 3: Abrir a base de dados de produtos
+#Passo 3: Abrir a base de dados de produtos + pandas library
+tabela = pandas.read_csv("produtos.csv") # Lê a base de dados de produtos a partir de um arquivo CSV
+print(tabela) # Exibe a tabela de produtos para verificar se foi carregada
+
 #Passo 4: Cadastrar um novo produto
 #Passo 5: Salvar as informações do produto
 #Passo 6: Repetir o processo até acabar a lista de produtos
