ce5b620abc0597d5f441d2800425a487a3a865d1
anaviboahoras <anavboahorasantos@gmail.com>
2026-03-06 14:40:04 -0300

    login

--- automacao.py
+++ automacao.py
@@ -3,8 +3,13 @@
 
 import pyautogui
 import time
+#pyautogui.click-> Clica em um ponto específico da tela
+#pyautogui.write-> Escreve um texto
+#pyautogui.press -> Pressiona uma tecla
+#pyautogui.hotkey('ctrl', 'alt', 't') Abre um atalho (ex: abrir terminal)
 
-pyautogui.PAUSE = 0.5 # Define uma pausa de 0.5 segundos entre as ações para evitar erros
+#Passo 1: Abrir o navegador e acessar o site
+pyautogui.PAUSE = 1 # Define uma pausa de 0.5 segundos entre as ações para evitar erros
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 pyautogui.press("win") # Pressiona a tecla "Windows" para abrir o menu iniciar
 pyautogui.write("Chrome") # Escreve "Google Chrome" para buscar o navegador
@@ -13,16 +18,17 @@ pyautogui.write(link) # Escreve o link do site
 pyautogui.press("enter") # Pressiona "Enter" para acessar o site
 time.sleep(3) # Aguarda 3 segundos para o site carregar
 
+#Passo 2: Fazer login
+time.sleep(1) # Aguarda 1 segundo
+pyautogui.click(x=515, y=403) # Clica no campo de email
+pyautogui.write("seuemail@gmail.com") # Escreve o email
+pyautogui.press("tab") # Pressiona "Tab" para ir para o campo de senha
+pyautogui.write("45667865424bl") # Escreve a senha
 
-#pyautogui.click-> Clica em um ponto específico da tela
-#pyautogui.write-> Escreve um texto
-#pyautogui.press -> Pressiona uma tecla
-#pyautogui.hotkey('ctrl', 'alt', 't') Abre um atalho (ex: abrir terminal)
+#Passo 3: Abrir a base de dados de produtos
+#Passo 4: Cadastrar um novo produto
+#Passo 5: Salvar as informações do produto
+#Passo 6: Repetir o processo até acabar a lista de produtos
 
-#Passo 1: Entrar no sistema da empresa
-#Passo 2: Abrir o navegador e acessar o site
-#Passo 3: Fazer login
-#Passo 4: Abrir a base de dados de produtos
-#Passo 5: Cadastrar um novo produto
-#Passo 6: Salvar as informações do produto
-#Passo 7: Repetir o processo até acabar a lista de produtos
+#time.sleep(5) # Aguarda 5 segundos para o usuário posicionar o mouse no local desejado para o clique
+#print(pyautogui.position()) # Retorna a posição atual do mouse (x, y) para ajudar a identificar as coordenadas corretas para os cliques.
